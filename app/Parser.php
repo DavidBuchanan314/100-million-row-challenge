f86e613f58990ca6cebcdc019a901c5169a88e7b
David Buchanan <d@vidbuchanan.co.uk>
2026-02-26 23:04:05 +0000

    datelutlut

--- app/Parser.php
+++ app/Parser.php
@@ -10,6 +10,18 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
+        $datelut = [];
+        for ($y = 1; $y <= 6; $y++) {
+            for ($mm = 1; $mm <= 12; $mm++) {
+                for ($dd = 1; $dd <= 31; $dd++) {
+                    $datelut[] = sprintf("%d-%02d-%02d", $y, $mm, $dd);
+                }
+            }
+        }
+        $datelutlut = array_flip($datelut);
+
+        print("done building date table\n");
+
         $map = [];
         $file = fopen($inputPath, 'r');
         $block = '';
@@ -25,9 +37,10 @@ final class Parser
             while ($idx < $blen) {
                 $comma = strpos($block, ',', $idx + 25);
                 $path = substr($block, $idx + 25, ($comma - $idx) - 25);
-                $date = ($block[$comma + 4]-1)*372 +
-                        (substr($block, $comma + 6, 2)-1)*31 +
-                        (substr($block, $comma + 9, 2)-1);
+                //$date = ($block[$comma + 4]-1)*372 +
+                //        (substr($block, $comma + 6, 2)-1)*31 +
+                //        (substr($block, $comma + 9, 2)-1);
+                $date = $datelutlut[substr($block, $comma + 4, 7)];
 
                 //print($path.":".$date."\n");
                 if (!isset($map[$path])) {
@@ -49,17 +62,6 @@ final class Parser
         }
 
         print("read $lines_read lines\n");
-
-        $datelut = [];
-        for ($y = 1; $y <= 6; $y++) {
-            for ($mm = 1; $mm <= 12; $mm++) {
-                for ($dd = 1; $dd <= 31; $dd++) {
-                    $datelut[] = sprintf("%d-%02d-%02d", $y, $mm, $dd);
-                }
-            }
-        }
-
-        print("done building date table\n");
 
         $outHandle = fopen($outputPath, 'wb');
         fwrite($outHandle, "{\n");
