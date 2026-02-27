76a5fe2445f7cd2d45f1913605cd8f05eecb3218
David Buchanan <d@vidbuchanan.co.uk>
2026-02-27 22:06:36 +0000

    Revert "stab in the dark"
    This reverts commit b0aecf7f4769031a54cba421c342203d43fd41cc.

--- app/Parser.php
+++ app/Parser.php
@@ -10,7 +10,7 @@ final class Parser
 {
     private const THREAD_COUNT = 8;
     private const READ_CHUNK_SZ = 0x10000;
-    private const NUM_DATES = 2604;
+    private const NUM_DATES = 2232;
 
     private function findSplitPoints(string $inputPath): array
     {
@@ -91,7 +91,7 @@ final class Parser
     public function parse(string $inputPath, string $outputPath): void
     {
         $datelut = [];
-        for ($y = 0; $y <= 6; $y++) {
+        for ($y = 1; $y <= 6; $y++) {
             for ($mm = 1; $mm <= 12; $mm++) {
                 for ($dd = 1; $dd <= 31; $dd++) {
                     $datelut[] = sprintf('%d-%02d-%02d', $y, $mm, $dd);
